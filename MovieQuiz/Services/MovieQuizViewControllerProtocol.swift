b1384b482054f02f72efaee5a570ba758e97f5f5
Aleksey <the-afro@yandex.ru>
2026-02-19 16:36:36 +0300

    Рефакторинг на MVP, покрытие Unit и UI тестами

---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//
+
+protocol MovieQuizViewControllerProtocol: AnyObject {
+    func show(quiz step: QuizStepViewModel)
+    func show(quiz result: QuizResultsViewModel)
+    
+    func highlightImageBorder(isCorrectAnswer: Bool)
+    
+    func showLoadingIndicator()
+    func hideLoadingIndicator()
+    
+    func setButtonsEnabled(_ isEnabled: Bool)
+    
+    func showNetworkError(message: String)
+}
